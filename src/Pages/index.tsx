85c95cd27ce01a0db985ff2f9cb2f26d9b47c282
Aloento <11802769+Aloento@users.noreply.github.com>
2023-08-08 18:21:58 +0200

    Layout & Style & Token

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,6 +1,10 @@
 import { ScaleButton } from "@telekom/scale-components-react";
+import { useRouter } from "~/Components/Router";
 
 export function EShopContent() {
+  const { Paths } = useRouter();
+  const path = Paths.at(0);
+
   return (
     <div>
       Click on the Vite and React logos to learn more
