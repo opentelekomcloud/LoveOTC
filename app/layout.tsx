18a36074b03fde75dacdf20e60a1d4b83a7762f9
Aloento <11802769+Aloento@users.noreply.github.com>
2023-08-08 12:45:46 +0200

    env

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body data-mode="light">{children}</body>
     </html>
   )
 }
