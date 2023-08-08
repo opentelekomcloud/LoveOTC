64ce7137e50412915a5937266699c015b799c36d
Aloento <11802769+Aloento@users.noreply.github.com>
2023-08-08 18:23:05 +0200

    [Front] Page Container (#25)
    * BrowserRouter
    
    * Layout & Style & Token

---
+++ src/Helpers/Styles.ts
@@ -0,0 +1,29 @@
+import { GriffelStyle } from "@griffel/react";
+
+/**
+ * @author Aloento
+ * @since 0.6.0 ML
+ * @version 0.1.0
+ */
+export const Flex: GriffelStyle = { display: "flex" };
+
+/**
+ * @author Aloento
+ * @since 0.6.0 ML
+ * @version 0.1.0
+ */
+export const Col: GriffelStyle = { flexDirection: "column" };
+
+/**
+ * @author Aloento
+ * @since 0.6.0 ML
+ * @version 0.1.0
+ */
+export const ColFlex: GriffelStyle = { ...Flex, ...Col };
+
+/**
+ * @author Aloento
+ * @since 0.6.0 ML
+ * @version 0.1.0
+ */
+export const NavH = 54;
