56bc74925013ed6fdb7557b7bc70bb2e7d549269
Aloento <11802769+Aloento@users.noreply.github.com>
2023-08-10 15:38:01 +0200

    [Front | Client | Detail] Variance Selector (#29)
    * Product
    
    * Product
    
    * Product

--- src/Pages/Product/index.tsx
+++ src/Pages/Product/index.tsx
@@ -1,13 +1,15 @@
-import { makeStyles, tokens } from "@fluentui/react-components";
+import { Button, Divider, Field, LargeTitle, SpinButton, Title3, ToggleButton, makeStyles, shorthands, tokens } from "@fluentui/react-components";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { BaseCard, ColFlex, Cover, Flex } from "~/Helpers/Styles";
+import { BaseCard, Col, ColFlex, Cover, Flex } from "~/Helpers/Styles";
 import { Lexical } from "~/Lexical";
+import demo from "./demo.json";
 
 const useStyle = makeStyles({
   img: {
     aspectRatio: "1",
     ...Cover,
+    ...shorthands.borderRadius(tokens.borderRadiusMedium)
   }
 })
 
@@ -20,29 +22,120 @@ export function Product() {
   const style = useStyle();
 
   return (
-    <div style={{
-      ...ColFlex,
-      rowGap: tokens.spacingVerticalXXL
-    }}>
+    <div style={ColFlex}>
       <div style={{
         ...Flex,
-        columnGap: tokens.spacingHorizontalXXL
+        columnGap: tokens.spacingHorizontalXXXL
       }}>
         <Carousel showArrows>
-          <img className={style.img} src="https://picsum.photos/1000/800" />
-          <img className={style.img} src="https://picsum.photos/800/1000" />
+          <img className={style.img} src="https://picsum.photos/550" />
+          <img className={style.img} src="https://picsum.photos/650" />
+          <img className={style.img} src="https://picsum.photos/500" />
+          <img className={style.img} src="https://picsum.photos/600" />
+          <img className={style.img} src="https://picsum.photos/700" />
+          <img className={style.img} src="https://source.unsplash.com/random" />
         </Carousel>
 
         <div style={{
           ...BaseCard,
-          flexBasis: `${100 / 1.732}%`,
-          flexShrink: 0
+          ...Col,
+          height: "fit-content",
+          flexBasis: "50%",
+          flexShrink: 0,
+          rowGap: tokens.spacingVerticalXL,
+          paddingTop: tokens.spacingVerticalM,
+          paddingLeft: tokens.spacingHorizontalXXL,
+          paddingRight: tokens.spacingHorizontalXXL,
+          paddingBottom: tokens.spacingHorizontalXXL
         }}>
+          <LargeTitle style={{
+            color: tokens.colorBrandForeground1
+          }}>
+            OTC SHIRT - GREY
+          </LargeTitle>
+
+          <Divider />
+
+          <div style={{
+            ...ColFlex,
+            rowGap: tokens.spacingVerticalM,
+          }}>
+            <Title3 style={{
+              color: tokens.colorBrandForeground1
+            }}>
+              SELECT SLEEVE: SHORT SLEEVE
+            </Title3>
+
+            <div style={{
+              ...Flex,
+              flexWrap: "wrap",
+              rowGap: tokens.spacingHorizontalS,
+              columnGap: tokens.spacingHorizontalM
+            }}>
+              <ToggleButton appearance="outline" checked style={{ borderColor: tokens.colorBrandForeground1 }}>Short Sleeve</ToggleButton>
+              <ToggleButton appearance="outline" style={{ borderColor: tokens.colorBrandForeground1 }}>Short Sleeve</ToggleButton>
+            </div>
+          </div>
+
+          <div style={{
+            ...ColFlex,
+            rowGap: tokens.spacingVerticalS,
+          }}>
+            <Title3 style={{
+              color: tokens.colorBrandForeground1
+            }}>
+              SELECT SIZE: SMALL
+            </Title3>
+
+            <div style={{
+              ...Flex,
+              flexWrap: "wrap",
+              rowGap: tokens.spacingHorizontalS,
+              columnGap: tokens.spacingHorizontalM
+            }}>
+              <ToggleButton appearance="outline" checked style={{ borderColor: tokens.colorBrandForeground1 }}>S</ToggleButton>
+              <ToggleButton appearance="outline" style={{ borderColor: tokens.colorBrandForeground1 }}>M</ToggleButton>
+              <ToggleButton appearance="outline" style={{ borderColor: tokens.colorBrandForeground1 }}>L</ToggleButton>
+              <ToggleButton appearance="outline" style={{ borderColor: tokens.colorBrandForeground1 }}>XL</ToggleButton>
+              <ToggleButton appearance="outline" style={{ borderColor: tokens.colorBrandForeground1 }}>XXL</ToggleButton>
+              <ToggleButton appearance="outline" style={{ borderColor: tokens.colorBrandForeground1 }}>XXXL</ToggleButton>
+            </div>
+          </div>
+
+          <Divider />
+
+          <div style={{
+            ...ColFlex,
+            rowGap: tokens.spacingVerticalS,
+          }}>
+            <Title3 style={{
+              color: tokens.colorBrandForeground1
+            }}>
+              QUANTITY
+            </Title3>
+
+            <div style={{
+              ...Flex,
+              justifyContent: "space-between",
+              columnGap: tokens.spacingHorizontalM
+            }}>
+              <Field>
+                <SpinButton appearance="underline" />
+              </Field>
+
+              <Button appearance="primary">ADD TO CART</Button>
+            </div>
+          </div>
 
         </div>
       </div>
 
-      <Lexical />
+      <div style={{
+        ...BaseCard,
+        padding: tokens.spacingHorizontalL
+      }}>
+        <Lexical Display State={JSON.stringify(demo.editorState)} />
+      </div>
     </div>
   )
 }
