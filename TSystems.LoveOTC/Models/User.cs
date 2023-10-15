ac33f15685adce49f262e3a4fe00fc2ea8bef27c
Aloento <11802769+Aloento@users.noreply.github.com>
2023-10-15 12:20:13 +0200

    User

---
+++ TSystems.LoveOTC/Models/User.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+namespace TSystems.LoveOTC.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+/**
+ * <remarks>
+ * @author Aloento
+ * @since 0.1.0
+ * @version 0.1.0
+ * </remarks>
+ */
+internal class User {
+    public Guid UserId { get; set; }
+
+    public string Name { get; set; }
+
+    [EmailAddress]
+    public string EMail { get; set; }
+
+    [Phone]
+    public string Phone { get; set; }
+
+    public string Address { get; set; }
+
+    public bool? Admin { get; set; }
+}
