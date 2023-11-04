3bcd4ec3737f82b152a822cfcb221f5193c49522
Aloento <11802769+Aloento@users.noreply.github.com>
2023-11-04 16:23:18 +0100

    Update ProductPatchName method to validate and update product name

--- TSystems.LoveOTC/AdminHub/Product/Patch.cs
+++ TSystems.LoveOTC/AdminHub/Product/Patch.cs
@@ -1,15 +1,31 @@
 namespace TSystems.LoveOTC.AdminHub;
 
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
 internal partial class AdminHub {
     /**
      * <remarks>
      * @author Aloento
      * @since 0.1.0
-     * @version 0.1.0
+     * @version 0.2.0
      * </remarks>
      */
     public async Task<bool> ProductPatchName(uint prodId, string name) {
-        throw new NotImplementedException();
+        var prop = typeof(Product).GetProperty(nameof(Product.Name))!;
+        var valid = prop.GetCustomAttribute<StringLengthAttribute>()!;
+
+        if (!valid.IsValid(name))
+            throw new HubException(valid.FormatErrorMessage("Name"));
+
+        var row = await this.Db.Products
+            .Where(x => x.ProductId == prodId)
+            .ExecuteUpdateAsync(x => x.SetProperty(p => p.Name, name));
+
+        return row > 0;
     }
 
     /**
