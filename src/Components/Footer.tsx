09f9e71dfe33dad31b92036e334cff7094d5cf11
Aloento <11802769+Aloento@users.noreply.github.com>
2024-01-24 15:04:44 +0100

    Update Footer component and font URLs

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Text, makeStyles, shorthands, tokens } from "@fluentui/react-components";
+import { Image, Text, makeStyles, shorthands, tokens } from "@fluentui/react-components";
 import { ColFlex, Flex, NavW } from "~/Helpers/Styles";
 
 /**
@@ -17,28 +17,32 @@ const useStyles = makeStyles({
     ...Flex,
     maxWidth: NavW,
     ...shorthands.margin(0, "auto"),
+    justifyContent: "space-between",
+    alignItems: "center"
   },
   white: {
-    color: "white"
+    color: "white",
+    lineHeight: tokens.lineHeightBase100
   },
   mag: {
-    color: "#ED538B"
+    color: "#e20074"
   },
-  text: {
-    ...ColFlex,
-    ...shorthands.margin(tokens.spacingVerticalXXXL, 0),
-  }
+  logo: {
+    ...Flex,
+    alignItems: "center",
+    columnGap: tokens.spacingHorizontalM
+  },
+  text: ColFlex
 });
 
 const txt = {
-  size: 800,
-  weight: "medium"
+  size: 400
 } as const;
 
 /**
  * @author Aloento
  * @since 0.1.0
- * @version 0.2.0
+ * @version 0.3.0
  */
 export function Footer(): JSX.Element {
   const style = useStyles();
@@ -46,18 +50,36 @@ export function Footer(): JSX.Element {
   return (
     <footer className={style.box}>
       <div className={style.main}>
-        <div className={style.text}>
-          <Text {...txt} className={style.mag}>
-            Open
-          </Text>
-          <Text {...txt} weight="medium" className={style.white}>
-            Telekom
-          </Text>
-          <Text {...txt} weight="medium" className={style.mag}>
-            Cloud
-          </Text>
+        <div className={style.logo}>
+          <Image src="/DTAG.svg" height="42px" />
+
+          <div className={style.text}>
+            <Text {...txt} className={style.mag}>
+              Open
+            </Text>
+            <Text {...txt} className={style.white}>
+              Telekom
+            </Text>
+            <Text {...txt} className={style.mag}>
+              Cloud
+            </Text>
+          </div>
         </div>
 
+        <div className={style.logo}>
+          <Text {...txt} className={style.white}>
+            Contact
+          </Text>
+          <Text {...txt} className={style.white}>
+            Disclaimer of liability
+          </Text>
+          <Text {...txt} className={style.white}>
+            Data privacy
+          </Text>
+          <Text {...txt} className={style.white}>
+            Imprint
+          </Text>
+        </div>
       </div>
     </footer>
   );
