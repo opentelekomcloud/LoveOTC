182a57997d33961f2a1a62d9aad077b965c1cabc
Aloento <Aloento@users.noreply.github.com>
2024-01-31 14:06:57 +0000

    deploy: 0d267d5afb06c3776f26ae307c73b5f740ac49ec

--- wwwroot/assets/Lazy-McMp2-4w.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./index-v1cM3gRd.js";import{r as s,j as e,ar as a}from"./vendor-QK9ZNwME.js";function p(r){return e.jsx(s.Suspense,{fallback:e.jsx(a,{}),children:e.jsx(o,{...r})})}const o=s.lazy(()=>t(()=>import("./index-0LzWEfdl.js").then(r=>r.q),__vite__mapDeps([0,1,2,3])));export{p as L};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-0LzWEfdl.js","assets/vendor-QK9ZNwME.js","assets/index-v1cM3gRd.js","assets/index-RvD1uQDq.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
