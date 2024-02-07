f6feb78b345ab9fd0205bda22e7366e7260a5461
Aloento <Aloento@users.noreply.github.com>
2024-02-07 20:20:32 +0000

    deploy: 544be091e1a4df14aeb90c5ddc5f9ee466c82fcd

---
+++ wwwroot/assets/Lazy-MBEdxg0Z.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./index-z0-ifvfy.js";import{r as e,j as s,as as a}from"./vendor-8Rlz0Fah.js";function p(r){return s.jsx(e.Suspense,{fallback:s.jsx(a,{}),children:s.jsx(o,{...r})})}const o=e.lazy(()=>t(()=>import("./index-b2E_lVtA.js").then(r=>r.q),__vite__mapDeps([0,1,2,3])));export{p as L};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/index-b2E_lVtA.js","assets/vendor-8Rlz0Fah.js","assets/index-z0-ifvfy.js","assets/index-RvD1uQDq.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
