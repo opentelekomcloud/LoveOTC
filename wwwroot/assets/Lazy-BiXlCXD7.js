fa8f127662a1bd242c7714a7088165f49a46cffc
Aloento <Aloento@users.noreply.github.com>
2024-03-04 14:12:26 +0000

    deploy: b3e2ec12a50f17ca404b11d6a428fe121ce42508

---
+++ wwwroot/assets/Lazy-BiXlCXD7.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./index-D2I0BiZs.js";import{r as s,j as e,aw as a}from"./vendor-Broez9QR.js";function p(r){return e.jsx(s.Suspense,{fallback:e.jsx(a,{}),children:e.jsx(o,{...r})})}const o=s.lazy(()=>t(()=>import("./index-BH24Shlj.js").then(r=>r.q),__vite__mapDeps([0,1,2,3])));export{p as L};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/index-BH24Shlj.js","assets/vendor-Broez9QR.js","assets/index-D2I0BiZs.js","assets/index-BG8PW5AO.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
--- wwwroot/assets/Lazy-DVQSYXbZ.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./index-BK4yBAUt.js";import{r as s,j as e,au as a}from"./vendor-DHqJYkmB.js";function p(r){return e.jsx(s.Suspense,{fallback:e.jsx(a,{}),children:e.jsx(o,{...r})})}const o=s.lazy(()=>t(()=>import("./index-D8PmRV2d.js").then(r=>r.q),__vite__mapDeps([0,1,2,3])));export{p as L};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/index-D8PmRV2d.js","assets/vendor-DHqJYkmB.js","assets/index-BK4yBAUt.js","assets/index-BG8PW5AO.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
